777a3c637747a4b76effe8ac96452b080e6e0133
jorgeburgueno <jorge.albert16@hotmail.com>
2025-02-13 15:02:31 -0700

    creacion de array de tareas

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 'use strict';
+//seleccion de elementos
 
-
-// Manejo de abrir y cerrar ventana de creacion de tareas
 const nuevaTarea = document.querySelector('.nueva-tarea');
 const btnCrearTarea = document.querySelector('.btn-crear');
 const btnCerrarVentana = document.querySelector('.btn-cerrar');
+const formTarea = document.querySelector('.form-tarea');
+
+
+
+// Manejo de abrir y cerrar ventana de creacion de tareas
 
 btnCrearTarea.addEventListener("click", () => {
     nuevaTarea.style.display = "block";    
@@ -16,4 +20,20 @@ btnCerrarVentana.addEventListener("click", () => {
 });
 
 
-//
+//Logica de creacion de tareas
+
+const tareas = []; 
+
+formTarea.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const tarea = {
+        name: document.querySelector("#name").value,
+        date: document.querySelector("#fecha_limite").value,
+        description: document.querySelector("#descripcion").value
+    };
+    tareas.push(tarea);
+    formTarea.reset();
+    nuevaTarea.style.display = "none";
+    console.log(tareas); 
+})
