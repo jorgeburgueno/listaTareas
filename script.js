9c43975d0946829c015f337bf70252f6e7149406
jorgeburgueno <jorge.albert16@hotmail.com>
2025-02-13 17:17:00 -0700

    agregadas tareas que se vean en display

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const btnCrearTarea = document.querySelector('.btn-crear');
 const btnCerrarVentana = document.querySelector('.btn-cerrar');
 const formTarea = document.querySelector('.form-tarea');
 
-
-
+const display = document.querySelector('.display');
 // Manejo de abrir y cerrar ventana de creacion de tareas
 
 btnCrearTarea.addEventListener("click", () => {
@@ -15,10 +14,8 @@ btnCrearTarea.addEventListener("click", () => {
 });
 
 btnCerrarVentana.addEventListener("click", () => {
-    nuevaTarea.style.display = "none";
-    console.log("asd");
+    nuevaTarea.style.display = "none";    
 });
-
 
 //Logica de creacion de tareas
 
@@ -36,4 +33,17 @@ formTarea.addEventListener('submit', (event) => {
     formTarea.reset();
     nuevaTarea.style.display = "none";
     console.log(tareas); 
-})
+
+    const newTask = document.createElement('div');
+    newTask.classList.add('task');
+    newTask.textContent = `${tarea.name}  ${tarea.date}  ${tarea.description}`;
+    display.appendChild(newTask);        
+        
+});
+
+//Mostrar tareas en display
+
+
+
+
+
